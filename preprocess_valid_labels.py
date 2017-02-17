42cba969892a1cf2361a89bb6d64ff1f574b4b81
shamit <shamitlal@yahoo.com>
2017-02-18 03:34:27 +0530

    preprocess validation labels (don't use for lmdb)

---
+++ preprocess_valid_labels.py
@@ -0,0 +1,13 @@
+text_file = open("dataset/valid.txt", "r")
+lines = text_file.read().split(',')
+names = []
+for i,pokestr in enumerate(lines,start=1):
+	pokename = pokestr.split('\n')[0]
+	names.append(pokename)
+
+test_file = open("dataset/valid_labels.txt","w")
+
+for i in names:
+	test_file.write(i)
+	test_file.write('\n')
+print names
